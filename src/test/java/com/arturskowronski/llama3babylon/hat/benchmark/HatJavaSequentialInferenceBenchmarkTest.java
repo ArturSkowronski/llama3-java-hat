f29a06dcf90c097086a9e18acc0c9ce8567d001e
Artur Skowronski <me@arturskowronski.com>
2026-02-20 12:14:22 +0100

    chore: tag inference benchmark tests as benchmark-chat

--- src/test/java/com/arturskowronski/llama3babylon/hat/benchmark/HatJavaSequentialInferenceBenchmarkTest.java
+++ src/test/java/com/arturskowronski/llama3babylon/hat/benchmark/HatJavaSequentialInferenceBenchmarkTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 @Tag("benchmark")
+@Tag("benchmark-chat")
 public class HatJavaSequentialInferenceBenchmarkTest {
 
     @Test
--- src/test/java/com/arturskowronski/llama3babylon/hat/benchmark/PlainJavaInferenceBenchmarkTest.java
+++ src/test/java/com/arturskowronski/llama3babylon/hat/benchmark/PlainJavaInferenceBenchmarkTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 @Tag("benchmark")
+@Tag("benchmark-chat")
 public class PlainJavaInferenceBenchmarkTest {
 
     @Test
